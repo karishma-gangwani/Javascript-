fb86f8dcb09e509add77c8d05ea09fbf61b8017c
karishma gangwani <77972514+karishma-gangwani@users.noreply.github.com>
2022-06-13 13:44:18 -0400

    modified

--- practice/footballBetting.js
+++ practice/footballBetting.js
@@ -50,13 +50,56 @@ const { team1, x: draw, team2 } = game.odds;
 // const {
 //   odds: { team1, x: draw, team2 },
 // } = game;
-console.log(team1, draw, team2);
+// console.log(team1, draw, team2);
 
 const printGoals = function (...players) {
   console.log(`${players}, ${players.length} goals were scored`);
 };
 
-printGoals(...game.scored);
+// when you want individual components from an array then use the spread operator.
+// printGoals(...game.scored);
 
-team1 < team2 && console.log("team1 is more likely to win");
-team1 > team2 && console.log("team2 is more likely to win");
+// team1 < team2 && console.log("team1 is more likely to win");
+// team1 > team2 && console.log("team2 is more likely to win");
+
+// using the .entries() method to iterate over index, element in the array scored nested in the object game.
+// for (const [i, player] of game.scored.entries()) {
+//   console.log(`goal ${i + 1}`, player);
+// }
+console.log(game.odds);
+console.log(Object.values(game.odds));
+console.log(Object.values(game.odds).length);
+
+// iterating over a nested object inside an object would require object.values
+let sum = 0;
+let avg = 0;
+for (const element of Object.values(game.odds)) {
+  sum += element;
+  avg = sum / Object.values(game.odds).length;
+}
+console.log(avg);
+
+console.log(
+  Object.values(game.odds).reduce((a, b) => a + b) /
+    Object.values(game.odds).length
+);
+
+console.log(
+  `\n odd victory of ${game.team1}: ${
+    Object.values(game.odds)[0]
+  }\n odd of draw: ${Object.values(game.odds)[1]}\n odd of victory ${
+    game.team2
+  }: ${Object.values(game.odds)[2]}`
+);
+
+// bonus question
+let scorers = {};
+for (const element of game.scored) {
+  scorers[element]
+    ? (scorers[element] += scorers[element])
+    : (scorers[element] = 1);
+}
+console.log(scorers);
+
+game.scorers = scorers;
+console.log(game);
