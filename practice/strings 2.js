bb0240f5734e6cff6e3bd5564f5b1f5d82a102df
karishma gangwani <77972514+karishma-gangwani@users.noreply.github.com>
2022-06-15 16:27:41 -0400

    new

---
+++ practice/strings 2.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+// console.log(flights.split('+'));
+
+for (const event of flights.split('+')) {
+  // destructuring the array.
+  const [type, from, to, time] = event.split(';');
+  // print the output in the desired format. and change the formatting of the individual components.
+  const output = `${type.startsWith('_Delayed') ? '🛑' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(50);
+  console.log(output);
+}
