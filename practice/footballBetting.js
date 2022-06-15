1d6e60da2e9e8aa0e450b49c7a4c319aeeb6f387
karishma gangwani <77972514+karishma-gangwani@users.noreply.github.com>
2022-06-15 13:39:37 -0400

    modified

--- practice/footballBetting.js
+++ practice/footballBetting.js
@@ -103,3 +103,27 @@ console.log(scorers);
 
 game.scorers = scorers;
 console.log(game);
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+const events = [...new Set(gameEvents.values())];
+
+gameEvents.delete(64);
+
+for (const [key, value] of gameEvents) {
+  key <= 45
+    ? console.log(`[First Half] ${key}: ${value}`)
+    : console.log(`[Second Half] ${key}: ${value}`);
+}
